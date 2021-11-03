8fe52b91f6958cd3aec8271801d3d357132c9e02
Jonathan Pappas <jonnyboypappas@gmail.com>
2021-11-03 10:36:54 -0700

    Tic Tac Toe App Complete

--- TicTacToe/TicTacToe/GameScene.swift
+++ TicTacToe/TicTacToe/GameScene.swift
@@ -10,82 +10,76 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    var boardSize = 9
+    var boardHeight: CGFloat = 568
+    var cellSize: CGFloat { boardHeight / CGFloat(boardSize) }
+        
     override func didMove(to view: SKView) {
+        backgroundColor = .white
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        let logo = SKSpriteNode.init(imageNamed: "TicTacToe")
+        logo.setScale((90) / logo.size.height)
+        logo.position.y = (self.size.height / 2) - 50
+        addChild(logo)
+        
+        var board = SKSpriteNode.init(imageNamed: "3x3Board")
+        if boardSize == 4 {
+            board = SKSpriteNode.init(imageNamed: "4x4Board")
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        if boardSize == 9 {
+            board = SKSpriteNode.init(imageNamed: "SuperBoard")
         }
+        addChild(board)
+        board.setScale((self.size.height - 200) / board.size.height)
+        boardHeight = board.size.height
+        print(board.size.height)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func tapped(_ pos: CGPoint) -> (x: Int, y: Int) {
+        var x = pos.x
+        x += cellSize * (CGFloat(boardSize) / 2)
+        print(Int(x) / Int(cellSize))
+        
+        var y = pos.y
+        y += cellSize * (CGFloat(boardSize) / 2)
+        print(Int(x) / Int(cellSize))
+        return ((Int(x) / Int(cellSize)), (Int(y) / Int(cellSize)))
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    var turn = true
+    func addSymbol(_ x: Int,_ y: Int) {
+        let s = SKSpriteNode.init(imageNamed: turn ? "x" : "o")
+        turn.toggle()
+        
+        s.setScale((cellSize * 0.5) / s.size.height)
+        
+        s.position.x = CGFloat(x) * cellSize
+        s.position.x -= boardHeight / 2
+        s.position.x += cellSize/2
+        
+        s.position.y = CGFloat(y) * cellSize
+        s.position.y -= boardHeight / 2
+        s.position.y += cellSize/2
+        
+        s.name = "\(x) \(y)"
+        
+        addChild(s)
     }
     
     override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
-    }
-    
-    override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+        let (x, y) = tapped(event.location(in: self))
+        
+        // for super board rules
+        if boardSize == 9, let (px, py) = previousSpot {
+            if (px % 3) == (x % 3), (py % 3) == (y % 3) { return }
+        }
+        
+        // Fixes bug of overlapping on a spot
+        if childNode(withName: "\(x) \(y)") == nil {
+            previousSpot = (x, y)
+            addSymbol(x, y)
         }
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    var previousSpot: (Int, Int)?
 }
